4ba36cdf49cbc9e37fec5892bbc2292dea5cc92b
nishuileabhain <nishuileabhain@gmail.com>
2024-01-03 22:36:54 +0000

    testdotnet

--- Program.cs
+++ Program.cs
@@ -5,4 +5,27 @@ Console.WriteLine("Enter a verb!");
 string verbentered;
 verbentered = Console.ReadLine();
 Console.WriteLine("You have entered: "+ verbentered);
+validateVerb(verbentered);
 
+
+     static void validateVerb(string i)
+    {
+        // vbisvalid = true;
+        string ending = i.Substring(i.Length -2);
+        switch(ending) 
+    {
+    case "ar":
+        Console.WriteLine("Its an AR verb.");
+        break;
+    case "er":
+        Console.WriteLine("Its an ER verb.");
+        break;
+    case "ir":
+        Console.WriteLine("Its an IR verb.");
+        break;
+    default:
+        Console.WriteLine("Not a verb.");
+        // vbisvalid = false;
+        break;
+        }
+    }
